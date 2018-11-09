f2374ba4bee2fd552ad0c753a3b9d18afb134ca3
Ewooral <boahen.cos@gmail.com>
2018-11-09 10:07:21 +0000

    updating

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="bootstrap.css">
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <link type="text/css" rel="stylesheet" href="ewooral.css">
+<title>
+Ewooral, Inc.
+</title>
+</head>
+<body>
+    <div class="overlay">
+    <div id="particles-js">
+    <div class="main">
+       <h1> CHURCH OF PENTECOST<br> <p style="font-size:19px; color:wheat;">Two Streams District</p><br></h1>
+        <img src="pentecost logo.gif" style="width:90px; height: 90px;"><br><br>
+<div class="col-6 card offset-3" style="font-size:18px;" > 
+    Ewooral <br>
+    <input type="text" placeholder="Enter Username" name="Uname" required>
+    <input type="password" placeholder="Enter Password" name="Pswd" required>
+   <a href="seemyinfo.php"> <input type="submit" value="Login" name="submit" ></a>
+</div>
+</div>
+</div>
+</div>
+<script  src="particles.js" type="text/JavaScript"></script>
+<script  src="app.js" type="text/JavaScript"></script>
+<script></script>
+</body>
+</html>
---
+++ seemyinfo.php
@@ -0,0 +1,5 @@
+<?php
+
+echo("Hi, you're all welcome")
+
+?>
